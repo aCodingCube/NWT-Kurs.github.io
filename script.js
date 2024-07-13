8ca9c67e4cef0f1043c876f7512c5aa800382480
aCodingCube <lorenzwittmann2008@gmail.com>
2024-07-13 13:04:15 +0200

    Reorganized CSS file

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// event listener for scrolling
+// for scroll animations
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll',window.scrollY / (document.body.offsetHeight - window.innerHeight));
-  }, false);
+}, false);
 
+// event listener for website finishing loading
+// for the rendering the count of entries
 window.addEventListener('DOMContentLoaded',function() {
     var numberOfEntries = document.querySelectorAll('.column').length;
     console.log(numberOfEntries);
