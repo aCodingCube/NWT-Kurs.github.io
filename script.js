e70328766373669dc0d1fb1d94a5e570c0c55008
aCodingCube <lorenzwittmann2008@gmail.com>
2024-07-13 12:53:41 +0200

    Updated CSS and Layout

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll',window.scrollY / (document.body.offsetHeight - window.innerHeight));
   }, false);
+
+window.addEventListener('DOMContentLoaded',function() {
+    var numberOfEntries = document.querySelectorAll('.column').length;
+    console.log(numberOfEntries);
+    this.document.getElementById("sub-heading").innerHTML += numberOfEntries;
+},false);
