d446302ccd9efe18508a711704c89b8ec22a1902
aCodingCube <lorenzwittmann2008@gmail.com>
2024-07-13 16:54:05 +0200

    Updated UI

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 // for scroll animations
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll',window.scrollY / (document.body.offsetHeight - window.innerHeight));
+    
+     
+}, false);
+
+window.addEventListener('scroll', () => {
+    var height = document.getElementById("upper-background").getBoundingClientRect.height;
+    var value = window.scrollY / height;
+    var finalValue;
+    if(value > 1)
+    {
+        finalValue = 1;
+    }
+    else{
+        finalValue = value;
+    }
+
+    document.body.style.setProperty('--newScroll',finalValue);
 }, false);
 
 // event listener for website finishing loading
